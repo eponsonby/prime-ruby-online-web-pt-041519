49585b851511f55b242c8a04b3b79d2061a8e626
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-20 18:11:59 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,22 @@
-# Add  code here!
+require 'pry'
+
+def prime?(integer)
+range = (2...integer).to_a
+
+  if integer <= 1
+    return false
+      elsif integer > 1 && integer <= 3
+        return true
+          range.each do |number|
+            if integer % number == 0
+              return false
+              break
+            elsif integer % number != 0
+            binding.pry
+            end
+          end
+      end
+end 
+
+prime?(4)
+  
