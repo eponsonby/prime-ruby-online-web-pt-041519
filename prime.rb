34c83e5df1c3d687603da9e75681961bb5105aa9
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-20 18:20:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,8 +5,9 @@ range = (2...integer).to_a
 
   if integer <= 1
     return false
-      elsif integer > 1 && integer <= 3
+      elsif integer == 2 || integer == 3
         return true
+        binding.pry
             range.each do |number|
               if integer % number == 0
                 return false
